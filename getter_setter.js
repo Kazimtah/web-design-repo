532433a5ee9666435923f412eb482be38c02ecff
Kazimtha <kazimhamid02@gmail.com>
2025-06-24 17:52:55 +0200

    Adding setter and getter method

---
+++ getter_setter.js
@@ -0,0 +1,41 @@
+function Programmar(name, preferedPrgromming){
+    let privateName = name;
+    Object.defineProperties(this, { 
+        'name': {
+            get: function(){
+                return privateName;
+
+            },
+            set: function(newName){
+                if(!newName){
+                    console.log("Name can not be empty");
+                    return;
+
+                }
+                privateName = newName;
+
+            }
+
+        }
+        
+
+    });
+    // prublic property
+    this.preferedPrgromming = preferedPrgromming;
+
+    // public method
+    this.writeCode = function(){
+        console.log(`${privateName} codes in ${this.preferedPrgromming}`);
+    }
+    let drinkConffee = function(){
+        console.log('Gulp...');
+    }
+    // public method that use closure 
+    this.startDay = function(){
+        drinkConffee();
+    }
+}
+const programmer = new Programmar('Alice', 'Javascript');
+console.log(programmer.name);
+programmer.name = 'Steven'
+console.log(programmer.name);
